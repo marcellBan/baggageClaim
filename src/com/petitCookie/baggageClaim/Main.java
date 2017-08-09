302851427ba8c99690b079ec707436ee2bb5a49f
GreywindGit <GreywindGit@users.noreply.github.com>
2017-08-09 11:36:50 +0200

    finish sorting class functions

--- src/com/petitCookie/baggageClaim/Main.java
+++ src/com/petitCookie/baggageClaim/Main.java
@@ -3,7 +3,6 @@ package com.petitCookie.baggageClaim;
 public class Main {
 
     public static void main(String[] args) {
-        Sorting lista = new Sorting();
-        lista.playSorting();
+
     }
 }
--- src/com/petitCookie/baggageClaim/Sorting.java
+++ src/com/petitCookie/baggageClaim/Sorting.java
@@ -1,9 +1,6 @@
 package com.petitCookie.baggageClaim;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Sorting {
 
@@ -14,10 +11,13 @@ public class Sorting {
         fullList = FileReader.readFile("random.txt");
     }
 
-    public void playSorting() {
+
+    public boolean playSorting() {
         List<String> words = createWordList();
-
-
+        List<Integer> result = new ArrayList<>(createResultList(words));
+        String[] userAnswer = getUserAnswer(words);
+        boolean success = checkUserAnswer(userAnswer, result);
+        return success;
     }
 
     private List createWordList() {
@@ -33,5 +33,39 @@ public class Sorting {
 
         return selectedWords;
     }
+
+    private List createResultList(List originalWords) {
+        List<String> sortedWords = new ArrayList<>(originalWords);
+        List<Integer> result = new ArrayList<>();
+        Collections.sort(sortedWords);
+        for (int i = 0; i < sortedWords.size(); i++) {
+            result.add(originalWords.indexOf(sortedWords.get(i)));
+        }
+        return result;
+    }
+
+
+    private String[] getUserAnswer(List wordsToSort) {
+        Scanner userAnswer = new Scanner(System.in);
+        for (Object word : wordsToSort) {
+            System.out.println(wordsToSort.indexOf(word) + " " + word);
+        }
+        System.out.print("Enter numbers in the right order (separated by space): ");
+        String answer = userAnswer.nextLine();
+        String[] numbers = answer.split(" ");
+        return  numbers;
+    }
+
+
+    private boolean checkUserAnswer(String[] answers, List correctAnswers) {
+       for (int i = 0; i < answers.length; i++) {
+           Integer number = new Integer(answers[i]);
+           if (!number.equals(correctAnswers.get(i))) {
+             return false;
+           }
+       }
+       return true;
+    }
+
 
 }
